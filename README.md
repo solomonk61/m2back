d7d35ae5efaa6d5eca801a2c9331034fbc6d5420
hamza <hamza@jsfactory.co>
2022-03-30 19:35:41 +0000

    added server url

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Strapi application
 
 A quick description of your strapi application
+
+> docker run -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres
--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("URL", "http://admin.example.com"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '0c09976d921ddf145d030691e510b77d'),
+      secret: env("ADMIN_JWT_SECRET", "0c09976d921ddf145d030691e510b77d"),
     },
   },
 });
